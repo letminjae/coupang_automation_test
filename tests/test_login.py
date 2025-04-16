c43ad3260c466961302d1ffd6dfd9eecce489434
차민재 <cmjj0824@naver.com>
2025-04-16 20:41:51 +0900

    [Feat] test_login.py
    - ID, PW 입력 후 로그인 서브밋 제출
    - assert로 정상 로그인 되어 myCoupang 요소 보이는지 확인

--- tests/test_login.py
+++ tests/test_login.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium_stealth import stealth
+import os
+from dotenv import load_dotenv
 
 chrome_options = Options()
 chrome_options.add_argument("--start-maximized")
@@ -25,3 +27,18 @@ driver.implicitly_wait(5)
 
 login_button = driver.find_element(By.CLASS_NAME, "login")
 login_button.click()
+
+load_dotenv() # .env 파일 로드
+
+id_input = driver.find_element(By.ID, "login-email-input")
+password_input = driver.find_element(By.ID, "login-password-input")
+
+id_input.send_keys(os.environ.get("COUPANG_ID"))
+password_input.send_keys(os.environ.get("COUPANG_PW"))
+
+login_submit = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+login_submit.click()
+driver.implicitly_wait(5)
+
+myCoupang = driver.find_element(By.ID, "myCoupang")
+assert myCoupang.is_displayed(), "로그인 성공"
