665eb6eb0ff7e3d895f567300daa666c612f6950
차민재 <cmjj0824@naver.com>
2025-04-19 18:26:21 +0900

    [ADD] test_login_fail_except_pw.py
    [TC-004] https://cmjj0824.atlassian.net/jira/software/projects/CEAT/boards/4?selectedIssue=CEAT-10
    - 비밀번호 입력 없이, 이메일만 입력
    - 에러메시지가 error_element에 출력되는지 확인

---
+++ tests/test_login_fail_except_pw.py
@@ -0,0 +1,47 @@
+# [TC-004] 비밀번호 입력 없이 로그인 시도
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium_stealth import stealth
+import os
+from dotenv import load_dotenv
+import time
+
+chrome_options = Options()
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+
+# Selenium Stealth 적용 (탐지 방지)
+stealth(driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Win32",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True)
+
+driver.get("https://www.coupang.com/")
+driver.implicitly_wait(5)
+
+login_button = driver.find_element(By.CLASS_NAME, "login")
+login_button.click()
+
+load_dotenv() # .env 파일 로드
+id_input = driver.find_element(By.ID, "login-email-input")
+
+id_input.send_keys(os.environ.get("COUPANG_ID"))
+
+login_submit = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+login_submit.click()
+
+time.sleep(1)
+
+error_element = driver.find_element(By.CSS_SELECTOR, ".member__message-area.member__message-area--error._loginPasswordError.login-fail-web-log-error-msg")
+
+assert "비밀번호를 입력해주세요." in error_element.text  # 비밀번호 입력 필드에 대한 에러 메시지 확인
+
+print("비밀번호 입력 제외 테스트 완료")
+driver.quit()
