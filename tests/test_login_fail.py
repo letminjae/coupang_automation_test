5aa46ac2907ecb4dcc8070ea38d0c10c5369cca8
차민재 <cmjj0824@naver.com>
2025-04-19 18:06:31 +0900

    [Add] test_login_fail.py
    [TC-002] https://cmjj0824.atlassian.net/jira/software/projects/CEAT/boards/4?selectedIssue=CEAT-6
    - 올바르지 않은 테스트 비밀번호 입력 시, 메인 페이지로 이동 되지 않고 로그인 실패

---
+++ tests/test_login_fail.py
@@ -0,0 +1,46 @@
+# [TC-002] 쿠팡 로그인 실패 테스트 케이스
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium_stealth import stealth
+import os
+from dotenv import load_dotenv
+import time
+
+chrome_options = Options()
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+
+# Selenium Stealth 적용 (탐지 방지)
+stealth(driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Win32",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True)
+
+driver.get("https://www.coupang.com/")
+driver.implicitly_wait(5)
+
+login_button = driver.find_element(By.CLASS_NAME, "login")
+login_button.click()
+
+load_dotenv() # .env 파일 로드
+id_input = driver.find_element(By.ID, "login-email-input")
+password_input = driver.find_element(By.ID, "login-password-input")
+
+id_input.send_keys(os.environ.get("COUPANG_ID"))
+password_input.send_keys("123456")  # 잘못된 비밀번호 입력
+
+login_submit = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+login_submit.click()
+
+time.sleep(3)
+
+assert driver.current_url != "https://www.coupang.com/" # 로그인 실패 시 URL이 변경되지 않아야 함
+print("로그인 실패 테스트 완료")
+driver.quit()
