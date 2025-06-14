c019d9cae514f107395a45be30cddf950f72233a
차민재 <cmjj0824@naver.com>
2025-06-14 14:36:55 +0900

    [Rename][Fix] 파일명 변경, 로그인 버튼 Fix 1. 테스트케이스+테스트내용.py 로 파일명 변경 2. 로그인 버튼 클래스명 사라짐으로인해 Fail, 클래스명 대신 title로 로그인 셀렉트

--- tests/login/test_login_001_success.py
+++ tests/login/test_login_001_success.py
@@ -1,4 +1,4 @@
-# [TC-LOGIN-002] 쿠팡 로그인 실패 테스트 케이스
+# [TC-LOGIN-001] 쿠팡 로그인 성공 테스트 케이스
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -6,13 +6,16 @@ from selenium.webdriver.chrome.options import Options
 from selenium_stealth import stealth
 import os
 from dotenv import load_dotenv
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 import time
 
 chrome_options = Options()
 chrome_options.add_argument("--start-maximized")
 chrome_options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(options=chrome_options)
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
 # Selenium Stealth 적용 (탐지 방지)
 stealth(driver,
@@ -26,21 +29,22 @@ stealth(driver,
 driver.get("https://www.coupang.com/")
 driver.implicitly_wait(5)
 
-login_button = driver.find_element(By.CLASS_NAME, "login")
+login_button = driver.find_element(By.CSS_SELECTOR, "a[title='로그인']")
 login_button.click()
 
 load_dotenv() # .env 파일 로드
+
 id_input = driver.find_element(By.ID, "login-email-input")
 password_input = driver.find_element(By.ID, "login-password-input")
 
 id_input.send_keys(os.environ.get("COUPANG_ID"))
-password_input.send_keys("123456")  # 잘못된 비밀번호 입력
+password_input.send_keys(os.environ.get("COUPANG_PW"))
 
 login_submit = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
 login_submit.click()
+driver.implicitly_wait(5)
 
-time.sleep(3)
-
-assert driver.current_url != "https://www.coupang.com/" # 로그인 실패 시 URL이 변경되지 않아야 함
-print("로그인 실패 테스트 완료")
+myCoupang = driver.find_element(By.CSS_SELECTOR, '#wa-mycoupang-link')
+assert myCoupang.is_displayed(), "로그인 성공"
+print("로그인 성공 테스트 완료")
 driver.quit()
--- tests/search/test_search_001_success.py
+++ tests/search/test_search_001_success.py
@@ -1,18 +1,20 @@
-# [TC-LOGIN-003] 이메일 입력 없이 로그인 시도
+# [TC-SEARCH-001] 쿠팡 검색 기능 테스트 케이스
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 from selenium_stealth import stealth
 import os
 from dotenv import load_dotenv
-import time
 
 chrome_options = Options()
 chrome_options.add_argument("--start-maximized")
 chrome_options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(options=chrome_options)
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
 # Selenium Stealth 적용 (탐지 방지)
 stealth(driver,
@@ -26,22 +28,17 @@ stealth(driver,
 driver.get("https://www.coupang.com/")
 driver.implicitly_wait(5)
 
-login_button = driver.find_element(By.CLASS_NAME, "login")
+login_button = driver.find_element(By.CSS_SELECTOR, "a[title='로그인']")
 login_button.click()
 
 load_dotenv() # .env 파일 로드
+
+id_input = driver.find_element(By.ID, "login-email-input")
 password_input = driver.find_element(By.ID, "login-password-input")
 
+id_input.send_keys(os.environ.get("COUPANG_ID"))
 password_input.send_keys(os.environ.get("COUPANG_PW"))
 
 login_submit = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
 login_submit.click()
-
-time.sleep(1)
-
-error_element = driver.find_element(By.CSS_SELECTOR, ".member__message-area.member__message-area--error._memberInputMessage.login-fail-web-log-error-msg")
-
-assert "아이디(이메일)를 입력해주세요." in error_element.text  # 이메일 입력 필드에 대한 에러 메시지 확인
-
-print("이메일 입력 제외 테스트 완료")
-driver.quit()
+driver.implicitly_wait(5)
