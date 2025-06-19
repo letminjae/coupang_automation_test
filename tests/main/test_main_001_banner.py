f6e426cf7e35cd99fce30d593ffa4643d5d2ed0f
차민재 <cmjj0824@naver.com>
2025-06-19 23:49:15 +0900

    [Chore] main/test_main_001_banner.py 테스트 안정성을 위한 쿠팡 페이지 로딩 후 time.sleep(1) 추가 WebDriverWait를 통한 동적 요소 안정화 코드 추가 - wait_for_visible_banner()

--- tests/main/test_main_001_banner.py
+++ tests/main/test_main_001_banner.py
@@ -19,25 +19,32 @@ driver = webdriver.Chrome(service=service, options=chrome_options)
 driver.get("https://www.coupang.com/")
 driver.implicitly_wait(5)
 
-# 현재 보여지는 배너 찾기
-def get_visible_banner_index():
-    banners = driver.find_elements(By.CSS_SELECTOR, ".main-today__bg")
-    for idx, banner in enumerate(banners):
-        try:
-            if banner.is_displayed():
-                return idx
-        except Exception as e:
-            print(f"배너 {idx} 확인 중 예외 발생 : {e}")
-    return -1
+# 슬라이더 요소 대기
+time.sleep(1)
 
-# 슬라이드 자동 전환 확인
-first_index = get_visible_banner_index()
+# 동적 요소 안정화 + 현재 보여지는 배너 찾기
+def wait_for_visible_banner():
+    def get_visible_banner_index():
+        banners = driver.find_elements(By.CSS_SELECTOR, ".main-today__bg")
+        for idx, banner in enumerate(banners):
+            try:
+                if banner.is_displayed():
+                    return idx
+            except Exception as E:
+                print("배너 요소 미확인.. 재시도")
+                continue
+        return -1
+    return WebDriverWait(driver, 10).until(lambda d: get_visible_banner_index() != -1 and get_visible_banner_index())
+
+# 슬라이드 자동 전환 테스트 시작
+first_index = wait_for_visible_banner()
 print("첫 배너 인덱스:", first_index)
 
 time.sleep(3)  # 슬라이드 자동 전환 대기
 
-second_index = get_visible_banner_index()
-print("다음 배너 인덱스:", second_index)
+second_index = wait_for_visible_banner()
+print("5초 후 다음 배너 인덱스:", second_index)
 
 assert first_index != second_index, "배너가 자동으로 전환되지 않았습니다."
+print("배너 자동 전환 테스트 PASS!")
 driver.quit()
