fba3dfacb97d6cdfd62f137d9b5cef2f15bfdfee
차민재 <cmjj0824@naver.com>
2025-07-02 19:42:39 +0900

    [ADD] config.py 설정 전체 설정값, 상수, 환경 변수 등을 추가하는 config 파일 생성

---
+++ utils/config.py
@@ -0,0 +1,15 @@
+# Webdriver 환경 설정 - 프로젝트 전체의 설정값, 상수, 환경 변수
+
+import random
+
+class Config:
+  BASE_URL = "https://www.coupang.com"
+  IMPLICIT_WAIT_TIME = 5 # driver.implicitly_wait()에 사용
+  EXPLICIT_WAIT_TIME = 10 # WebDriverWait에 사용
+
+  # WebDriver 옵션 관련
+  USER_AGENTS = [
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.5993.117 Safari/537.36"
+  ]
