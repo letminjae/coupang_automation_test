5736ecdea0cd7ad98bb93b83f11933bb4b05e6cc
차민재 <cmjj0824@naver.com>
2025-07-09 21:28:20 +0900

    [CHORE] main_page 로케이터 및 메서드 추가 배너 관련 메서드인 get_visible_banner_index, wait_for_banner_transition 함수 추가

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,12 +2,15 @@
 
 from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from utils.config import Config
+import time
 
 # Main Page 전용 로케이터
 class MainPageLocators:
     LOGIN_BUTTON = (By.CSS_SELECTOR, "a[title='로그인']")
     MY_COUPANG_LINK = (By.ID, "wa-mycoupang-link")
+    MAIN_TODAY_BANNERS = (By.CSS_SELECTOR, ".main-today__bg") # 모든 배너 요소
 
 # Main Page 전용 메서드
 class MainPage(BasePage):
@@ -26,3 +29,31 @@ class MainPage(BasePage):
     def get_my_coupang_link_text(self):
         """'마이쿠팡' 링크의 텍스트를 반환"""
         return self.get_element_text(MainPageLocators.MY_COUPANG_LINK)
+    
+    def get_visible_banner_index(self):
+        """
+        현재 화면에 보이는 배너의 인덱스를 반환
+        배너 요소가 완전히 로딩될 때까지 대기
+        """
+        self.wait.until(EC.presence_of_all_elements_located(MainPageLocators.MAIN_TODAY_BANNERS))
+        banners = self.find_elements(MainPageLocators.MAIN_TODAY_BANNERS)
+        for idx, banner in enumerate(banners):
+            try:
+                if banner.is_displayed():
+                    return idx
+            except Exception as E:
+                print("배너 요소 미확인.. 재시도")
+                continue
+        return -1
+    
+    def wait_for_banner_transition(self, initial_index, timeout=10):
+        """
+        배너가 초기 인덱스에서 다음 인덱스로 전환될 때까지 대기
+        """
+        start_time = time.time()
+        while time.time() - start_time < timeout:
+            current_index = self.get_visible_banner_index()
+            if current_index != -1 and current_index != initial_index:
+                return current_index
+            self.random_sleep(0.5, 1)
+        return initial_index
