5b3b107d312d6755b54e9e59eade3f388d209158
차민재 <cmjj0824@naver.com>
2025-08-04 19:55:30 +0900

    [UPDATE] Jenkins 환경변수 설정 로컬, 젠킨스 환경에서의 환경변수 가져오는 코드 분리

--- utils/config.py
+++ utils/config.py
@@ -1,7 +1,7 @@
 # Webdriver 환경 설정 - 프로젝트 전체의 설정값, 상수, 환경 변수
 
 import random
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 import os
 
 class Config:
@@ -17,9 +17,26 @@ class Config:
   ]
 
   def __init__(self):
-      load_dotenv()
-      self.COUPANG_ID = os.environ.get("COUPANG_ID")
-      self.COUPANG_PW = os.environ.get("COUPANG_PW")
+        # 1. Jenkins 환경 변수를 먼저 가져오기
+        self.COUPANG_ID = os.environ.get("COUPANG_ID")
+        self.COUPANG_PW = os.environ.get("COUPANG_PW")
+
+        # 2. Jenkins 환경 변수가 없는 경우에만, .env 파일을 로드
+        # 로컬 개발 환경용
+        if self.COUPANG_ID is None or self.COUPANG_PW is None:
+            dotenv_path = os.path.join(os.getcwd(), '.env')
+            if os.path.exists(dotenv_path):
+                load_dotenv(dotenv_path=dotenv_path, override=True, verbose=True) # override=True는 기존 환경변수 덮어쓰기
+                self.COUPANG_ID = os.environ.get("COUPANG_ID")
+                self.COUPANG_PW = os.environ.get("COUPANG_PW")
+            else:
+                print(f"env 파일을 찾을 수 없습니다. - {dotenv_path}. 환경 변수를 설정해주세요")
+
+        # 디버깅을 위한 최종 값 확인 (나중에 삭제)
+        if self.COUPANG_ID is None:
+            print("쿠팡 아이디가 None입니다. 환경 변수를 확인해주세요.")
+        if self.COUPANG_PW is None:
+            print("쿠팡 비밀번호가 None입니다. 환경 변수를 확인해주세요.")
 
 # Config 인스턴스 생성
 config_instance = Config()
